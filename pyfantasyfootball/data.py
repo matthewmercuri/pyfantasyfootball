2852b492d123d630a28e8182b8c6b5592122012e
Matthew Mercuri <mercurimatthew@gmail.com>
2020-10-23 09:16:07 -0400

    initial commit

---
+++ pyfantasyfootball/data.py
@@ -0,0 +1,117 @@
+from bs4 import BeautifulSoup
+import os
+import pandas as pd
+import requests
+
+
+YEAR = '2020'
+FANT_TABLE_URL = 'https://www.pro-football-reference.com/years/'
+
+
+class Data:
+    def __init__(self):
+        self.cwd = os.getcwd()
+
+    def fantasy_table(self, year=YEAR, save=False):
+        # using bs4 to scrape fantasy table from bs4
+        url = f'{FANT_TABLE_URL}{year}/fantasy.htm'
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'lxml')
+        fant_table = soup.find('table')  # selecting table
+
+        # finding links to players' profile page
+        player_links_td = soup.find_all("td", {"data-stat": "player"})
+        player_links = {}
+        for td in player_links_td:
+            _a = td.find("a", href=True)
+            player_links[_a.text.strip().upper()] = _a['href']
+
+        # passing html to pandas
+        fant_df = pd.read_html(str(fant_table), index_col=0)[0]
+
+        # cleaning df
+        fant_df.columns = fant_df.columns.droplevel(0)  # rem double headers
+        fant_df = fant_df[fant_df['Player'] != 'Player']
+        fant_df = fant_df.fillna(0)  # filling empty cells with 0
+        new_dtypes = {'Age': int, 'G': int, 'GS': int, 'Cmp': int,
+                      'Yds': float, 'TD': int, 'Int': int, 'Att': int,
+                      'Y/A': float, 'Tgt': int, 'Y/R': float, 'Fmb': int,
+                      'FL': int, '2PM': int, '2PP': int, 'FantPt': float,
+                      'PPR': float, 'DKPt': float, 'FDPt': float,
+                      'PosRank': int, 'OvRank': int, 'VBD': int, 'Rec': int}
+        fant_df = fant_df.astype(new_dtypes)  # assigning proper types to df
+        fant_df['FantPos'] = fant_df.apply(lambda x:
+                                           str(x['FantPos']).upper().strip(),
+                                           axis=1)
+        fant_df['Player'] = fant_df.apply(lambda x:
+                                          str(x['Player']).upper().strip(),
+                                          axis=1)
+        fant_df['Tm'] = fant_df.apply(lambda x:
+                                      str(x['Tm']).upper().strip(), axis=1)
+        fant_df.set_index('Player', inplace=True)
+        fant_df.sort_values(by='FantPt', ascending=False, inplace=True)
+
+        # adding player links col
+        fant_df['Prof Link'] = 'None'
+        for player in fant_df.index:
+            fant_df.at[player, 'Prof Link'] = player_links[player]
+
+        # adding additional stats if preferred
+        fant_df = self._additional_stats_fant_df(fant_df)
+
+        if save is True:
+            # creating folder to store results if it does not exist
+            try:
+                os.mkdir(os.path.join(self.cwd, 'FantasyStandings'))
+            except Exception as e:
+                print(e)
+            fant_df.to_csv('FantasyStandings/fantrankings.csv')
+
+        return fant_df
+
+    def save_current_pos_rankings(self):
+        fant_df = self.fantasy_table()
+
+        # creating new dfs for each position
+        qb_df = fant_df[fant_df['FantPos'] == 'QB']
+        wr_df = fant_df[fant_df['FantPos'] == 'WR']
+        rb_df = fant_df[fant_df['FantPos'] == 'RB']
+        te_df = fant_df[fant_df['FantPos'] == 'TE']
+
+        # creating folder to store results if it does not exist
+        try:
+            os.mkdir(os.path.join(self.cwd, 'FantasyStandings'))
+        except Exception as e:
+            print(e)
+
+        # saving locally to csv files
+        qb_df.to_csv('FantasyStandings/qbfantstandings.csv')
+        wr_df.to_csv('FantasyStandings/wrfantstandings.csv')
+        rb_df.to_csv('FantasyStandings/rbfantstandings.csv')
+        te_df.to_csv('FantasyStandings/tefantstandings.csv')
+
+    def players(self, year=YEAR):
+        year = str(year)
+        fant_df = self.fantasy_table(year=year)
+
+        # creating a json like dictionary containing player, pos,
+        # and gamelog link
+        players_dict = {}
+        players = fant_df.index
+
+        for player in players:
+            pos = fant_df.loc[player]['FantPos']
+            link = fant_df.loc[player]['Prof Link']
+            players_dict[player] = {'Position': pos, 'Profile Link': link}
+
+        return players_dict
+
+    def _additional_stats_fant_df(self, df):
+        df['FantPt/G'] = df['FantPt'] / df['G']
+        return df
+
+
+Data = Data()
+print(Data.fantasy_table())
+# Data.save_current_pos_rankings()
+# print(Data.players())
